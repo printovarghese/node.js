fcd55d3794ccc1658637e094cb105e847f259339
Printo Varghese <printovarghese09@gmail.com>
2026-04-06 12:39:22 +0530

    updated message text

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 My Node App is Running (CI/CD Ready)');
+  res.send('🚀 My Node App is Running (CI/CD Ready for nodejs app)');
 });
 
 app.listen(PORT, () => {
