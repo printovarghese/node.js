8f45dddd648f2bbc7c63ab8620c2b51664628691
Printo Varghese <printovarghese09@gmail.com>
2026-04-20 17:12:35 +0530

    first commit new chages ok-ok

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 My Node App is Running (CI/CD Ready for nodejs gsoft )');
+  res.send('🚀 My Node App is Running (CI/CD Ready for nodejs gsoft-printo )');
 });
 
 app.listen(PORT, () => {
